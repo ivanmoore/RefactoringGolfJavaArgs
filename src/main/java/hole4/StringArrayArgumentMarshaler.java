863ef579f9e38e4af6cba28e0158b2a4cf48dc5b
Ivan Moore <ivan@teamoptimization.com>
2020-09-03 18:21:45 +0100

    setting up for third hole

---
+++ src/main/java/hole4/StringArrayArgumentMarshaler.java
@@ -0,0 +1,27 @@
+package hole4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static hole4.ArgsException.ErrorCode.MISSING_STRING;
+
+public class StringArrayArgumentMarshaler implements ArgumentMarshaler {
+    private List<String> strings = new ArrayList<>();
+
+    public static String[] getValue(ArgumentMarshaler am) {
+        if (am != null && am instanceof StringArrayArgumentMarshaler) {
+            return ((StringArrayArgumentMarshaler) am).strings.toArray(new String[0]);
+        }
+        return new String[0];
+    }
+
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        try {
+            strings.add(currentArgument.next());
+        } catch (NoSuchElementException e) {
+            throw new ArgsException(MISSING_STRING);
+        }
+    }
+}
